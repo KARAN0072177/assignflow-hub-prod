09c9171cd712ca4a6a2ac418e60f994b6d1b929a
Karan <karanvani2003@gmail.com>
2026-01-29 19:36:08 +0530

    final project

--- client/src/services/auditLog.api.ts
+++ client/src/services/auditLog.api.ts
@@ -4,6 +4,7 @@ const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export interface AuditLog {
+  actorId: any;
   _id: string;
   actorRole: string;
   action: string;
